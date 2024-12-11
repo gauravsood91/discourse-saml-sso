32b4cd9560682e2e03ced6ed114856e0fc339a58
gauravsood91 <gauravsood.91@gmail.com>
2024-12-11 13:23:53 +0530

    trying without gems

--- plugin.rb
+++ plugin.rb
@@ -5,10 +5,9 @@
 
 #require_dependency 'auth/oauth2_authenticator'
 
-gem 'macaddr', '1.7.2'
+#gem 'macaddr', '1.7.2'
 #gem 'uuid', '2.3.9'
-gem 'ruby-saml', '1.17.0'
-gem "omniauth-saml", '2.2.1'
+#gem 'ruby-saml', '1.17.0'
 #gem "systemu", '2.6.5'
 #gem 'saml2ruby', '1.1.0'
 
